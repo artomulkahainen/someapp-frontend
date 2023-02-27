bbcb58df0a7226ca2d5ea1d00c8f5385d243710f
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2023-02-27 18:59:43 +0200

    Login implementation test fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders frontpage', () => {
-    render(<App />);
+    render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
+    );
     const linkElement = screen.getByText(/GimmeVibe/i);
     expect(linkElement).toBeInTheDocument();
 });
